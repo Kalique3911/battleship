77e7bfa324d7dfe639dd968c5ed04d0c36052eef
Kalique <kalique@gmail.com>
2024-02-05 22:06:11 +0200

    sea grid added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 document.getElementById("head").innerHTML = "Battleship"
+
+let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+letters.map(l => {
+    let letter = document.createElement('div')
+    letter.innerHTML = l
+    document.getElementById('letters').appendChild(letter)
+})
+
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+numbers.map(n => {
+    let number = document.createElement('div')
+    number.innerHTML = n
+    number.addEventListener("click", () => {alert(`you clicked number ${n}`)})
+    document.getElementById('numbers').appendChild(number)
+})
+
+numbers.map(n => {
+    letters.map(l => {
+        let sqr = document.createElement('div')
+        // sqr.innerHTML = l + n
+        sqr.addEventListener("click", () => {alert(`you clicked square ${l + n}`)})
+        document.getElementById('squares').appendChild(sqr)
+    })
+})
