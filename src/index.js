b5229789ae87ef1f0c66d7060887537e946a7389
Kalique <kalique@gmail.com>
2024-02-07 22:07:30 +0200

    embedding placing of ships

--- src/index.js
+++ src/index.js
@@ -1,69 +1,83 @@
 let placedShips = []
+let occupiedSquares = []
 let shipSize = 3
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-document.getElementById("head").innerHTML = "Battleship"
+document.getElementById('head').innerHTML = 'Battleship'
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 letters.map(l => {
-    let letter = document.createElement("div")
+    let letter = document.createElement('div')
     letter.innerHTML = l
-    document.getElementById("firstLetters").appendChild(letter)
+    document.getElementById('firstLetters').appendChild(letter)
 })
 
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
 numbers.map(n => {
     let number = document.createElement('div')
     number.innerHTML = n
-    number.addEventListener("click", () => {alert(`you clicked number ${n}`)})
-    document.getElementById("firstNumbers").appendChild(number)
+    number.addEventListener('click', () => {
+        alert(`you clicked number ${n}`)
+    })
+    document.getElementById('firstNumbers').appendChild(number)
 })
 
 numbers.map(n => {
     letters.map(l => {
-        let sqr = document.createElement("div")
-        sqr.addEventListener("click", () => {
+        let sqr = document.createElement('div')
+        sqr.addEventListener('click', () => {
             alert(`you clicked square ${l + n}`)
         }, {once: true})
-        document.getElementById("firstSquares").appendChild(sqr)
+        document.getElementById('firstSquares').appendChild(sqr)
     })
 })
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let secondLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+let secondLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 secondLetters.map(l => {
-    let letter = document.createElement("div")
+    let letter = document.createElement('div')
     letter.innerHTML = l
-    document.getElementById("secondLetters").appendChild(letter)
+    document.getElementById('secondLetters').appendChild(letter)
 })
 
-let secondNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+let secondNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
 secondNumbers.map(n => {
     let number = document.createElement('div')
     number.innerHTML = n
-    number.addEventListener("click", () => {alert(`you clicked number ${n}`)})
-    document.getElementById("secondNumbers").appendChild(number)
+    number.addEventListener('click', () => {
+        alert(`you clicked number ${n}`)
+    })
+    document.getElementById('secondNumbers').appendChild(number)
 })
 
 secondNumbers.map(n => {
     secondLetters.map(l => {
-        let sqr = document.createElement("div")
-        sqr.addEventListener("click", () => {
+        let sqr = document.createElement('div')
+        sqr.id = `${l}${n}`
+        let listener = () => {
             let c = 0
             while (shipSize > c) {
-                placedShips.push(`${l}${Number(n) + c}`)
-                c++
+                if (!(occupiedSquares.some(s => s === document.getElementById(`${l}${Number(n) + c}`).id))) {
+                    placedShips.push(`${l}${Number(n) + c}`)
+                    occupiedSquares.push(`${l}${Number(n) + c}`)
+                    if ((secondLetters.indexOf(l) - 1) > -1) {
+                        occupiedSquares.push(`${secondLetters[secondLetters.indexOf(l) - 1]}${Number(n) + c}`)
+                    }
+                    c++
+                } else {
+                    break
+                }
             }
-
-        }, {once: true})
-        document.getElementById("secondSquares").appendChild(sqr)
+        }
+        sqr.addEventListener('click', listener, {once: true})
+        document.getElementById('secondSquares').appendChild(sqr)
     })
 })
